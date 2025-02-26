1e8a350affefeca686aa0d172786a60a8201d512
CreGecko <ckdgusss@gmail.com>
2025-02-27 03:59:09 +0900

    fix python file - edit server.py

--- generate_question/server.py
+++ generate_question/server.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
 from typing import List
-from generate_question.generate_question import generate_question_with_lang_chain, return_keyword
+from generate_question.generate_question import generate_question_with_lang_chain
+from generate_question.create_factor import create_keywords
 
 app = FastAPI()
 
 
 @app.get("/generate_questions/")
-async def generate_questions(keyword: List[str], type_value: str, source_value: str, date: str):
+async def generate_questions(keyword: List[str], type_value: str, source_value: str, period: str):
     question = []
-    keywords = return_keyword(source_value, keyword)
+    k_value = create_keywords(source_value, keyword)
 
-    for k in keywords:
-        json_temp = generate_question_with_lang_chain(type_value, k, date, source_value)
+    for k in k_value:
+        json_temp = generate_question_with_lang_chain(type_value, k, period, source_value)
         json_temp['keyword'] = k
         json_temp['type'] = type_value
         json_temp['source'] = source_value
